092f259e757bbc3d896241208d770028b8fe0afa
Yuxi Long <longyuxi@live.com>
2023-08-18 12:27:18 -0400

    biolip test running

---
+++ gbr/gbr_inference/biolip_test/job_wrapper.py
@@ -0,0 +1,65 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent / '..'))
+sys.path.append(str(Path(__file__).parent / '..' / '..'))
+sys.path.append(str(Path(__file__).parent / '..' / '..' / '..'))
+from db import DB
+
+import argparse
+import traceback
+from tqdm import tqdm
+import inference
+
+import analyze_gbr_importance
+
+def job(key):
+    # The part where the job actually runs, given df and idx as input
+
+    # Each entry in the redis database should be a dictionary in the following form
+
+    d = DB.hgetall(key)
+    print(d)
+
+    protein_file = d['Protein File']
+    ligand_file = d['Ligand File']
+
+    predictions = inference.predict(protein_file, ligand_file)
+
+    # Save results
+    DB.hset(key, mapping={
+        **d,
+        **predictions
+    })
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--key')
+
+    args = parser.parse_args()
+    key = args.key
+
+    print('key', key)
+    print('job started')
+    d = DB.hgetall(key)
+    d['attempted'] = 'True'
+    DB.hset(key, mapping=d)
+
+    try:
+        job(key)
+        print('job finished')
+        d = DB.hgetall(key)
+        d['finished'] = 'True'
+        d['error'] = 'False'
+        DB.hset(key, mapping=d)
+        print('job success')
+
+    except Exception as err:
+        print(Exception, err)
+        print(traceback.format_exc())
+        print('job error')
+
+        d = DB.hgetall(key)
+        d['finished'] = 'True'
+        d['error'] = 'True'
+        DB.hset(key, mapping=d)
